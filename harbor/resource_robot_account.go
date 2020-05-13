b3678544dd3193db724ab91e7acde14ffc4d4434
Ben Osman <dev@smoothify.com>
2020-05-13 12:17:59 +0100

    refactor robot account creation

--- harbor/resource_robot_account.go
+++ harbor/resource_robot_account.go
@@ -3,10 +3,9 @@ package harbor
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
-
 	"github.com/benosman/terraform-provider-harbor/client"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"path"
 )
 
 var pathRobot string = "/api/v2.0/projects"
@@ -15,6 +14,7 @@ type robot struct {
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	Access      []access `json:"access"`
+	ExpiresAt   int      `json:"expires_at"`
 }
 
 type access struct {
@@ -23,9 +23,10 @@ type access struct {
 }
 
 type robotAccount struct {
-	Token   string `json:"token,omitempty"`
-	RobotID int    `json:"id,omitempty"`
-	Name    string `json:"name,omitempty"`
+	Token     string `json:"token,omitempty"`
+	RobotID   int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	ExpiresAt int    `json:"expires_at,omitempty"`
 }
 
 func resourceRobotAccount() *schema.Resource {
@@ -46,20 +47,43 @@ func resourceRobotAccount() *schema.Resource {
 				Optional: true,
 				Default:  nil,
 			},
-			"action": {
-				Type:     schema.TypeString,
+			"allow_push": {
+				Type:     schema.TypeBool,
 				Optional: true,
-				Default:  "pull",
+				Default:  false,
 				ForceNew: true,
+			},
+			"allow_helm_pull": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				ForceNew: true,
+			},
+			"allow_helm_push": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				ForceNew: true,
+			},
+			"never_expires": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				ForceNew: true,
+			},
+			"expires_at": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+				ComputedWhen: []string{
+					"never_expire",
+				},
 			},
 			"token": {
 				Type:      schema.TypeString,
 				Computed:  true,
 				Sensitive: true,
-			},
-			"robot_id": {
-				Type:     schema.TypeString,
-				Computed: true,
 			},
 		},
 		Create: resourceRobotAccountCreate,
@@ -69,25 +93,83 @@ func resourceRobotAccount() *schema.Resource {
 	}
 }
 
+func resourceRobotSetToState(d *schema.ResourceData, robot robotAccount) {
+	d.Set("expires_at", robot.ExpiresAt)
+}
+
 func resourceRobotAccountCreate(d *schema.ResourceData, m interface{}) error {
 	apiClient := m.(*client.Client)
 	projectid := d.Get("project_id").(string)
 
 	url := pathRobot + "/" + projectid + "/robots"
-	resource := "/project/" + projectid + "/repository"
+
+	resourceBase := "/project/" + projectid
+	resourceRepo := resourceBase + "/repository"
+	resourceHelmPull := resourceBase + "/helm-chart"
+	resourceHelmPush := resourceBase + "/helm-chart-version"
+
+	var accessList []access
+
+	if d.Get("allow_push").(bool) {
+		accessList = append(accessList, access{
+			Action: "push",
+			Resource: resourceRepo,
+		})
+	}
+
+	if d.Get("allow_helm_pull").(bool) {
+		accessList = append(accessList, access{
+			Action: "read",
+			Resource: resourceHelmPull,
+		})
+	}
+
+	if d.Get("allow_helm_push").(bool) {
+		accessList = append(accessList, access{
+			Action: "create",
+			Resource: resourceHelmPush,
+		})
+	}
+
+	if d.Get("never_expires").(bool) {
+		_ = d.Set("expires_at", -1)
+	}
 
 	body := robot{
 		Name:        d.Get("name").(string),
 		Description: d.Get("description").(string),
-		Access: []access{
-			access{
-				Action:   d.Get("action").(string),
-				Resource: resource,
-			},
-		},
+		Access:      accessList,
+		ExpiresAt:   d.Get("expires_at").(int),
+	}
+	/*
+	{"access":[{"action":"push","resource":"/project/7/repository"},{"action":"read","resource":"/project/7/helm-chart"},{"action":"create","resource":"/project/7/helm-chart-version"}],"expires_at":-1,"name":"drone"}
+	 */
+	resp, err := apiClient.SendRequestFull("POST", url, body, 201)
+	if err != nil {
+		return err
 	}
 
-	resp, err := apiClient.SendRequest("POST", url, body, 201)
+	var jsonData robotAccount
+
+	err = json.Unmarshal([]byte(resp.Body), &jsonData)
+	if err != nil {
+		return fmt.Errorf("[ERROR] Unable to unmarshal: %s", err)
+	}
+
+	location := resp.Headers.Get("location")
+	_, robotId := path.Split(location)
+	d.SetId(robotId)
+
+	d.Set("token", jsonData.Token)
+	return resourceRobotAccountRead(d, m)
+}
+
+func resourceRobotAccountRead(d *schema.ResourceData, m interface{}) error {
+	apiClient := m.(*client.Client)
+	projectid := d.Get("project_id").(string)
+	url := pathRobot + "/" + projectid + "/robots/" + d.Id()
+
+	resp, err := apiClient.SendRequest("GET", url, nil, 200)
 	if err != nil {
 		return err
 	}
@@ -99,38 +181,8 @@ func resourceRobotAccountCreate(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("[ERROR] Unable to unmarshal: %s", err)
 	}
 
-	d.SetId(randomString(15))
-	d.Set("token", jsonData.Token)
-	return resourceRobotAccountRead(d, m)
-}
+	resourceRobotSetToState(d, jsonData)
 
-func resourceRobotAccountRead(d *schema.ResourceData, m interface{}) error {
-	apiClient := m.(*client.Client)
-	projectid := d.Get("project_id").(string)
-	name := d.Get("name").(string)
-	url := pathRobot + "/" + projectid + "/robots"
-
-	resp, err := apiClient.SendRequest("GET", url, nil, 200)
-	if err != nil {
-		return err
-	}
-
-	var jsonData []robotAccount
-
-	err = json.Unmarshal([]byte(resp), &jsonData)
-	if err != nil {
-		return fmt.Errorf("[ERROR] Unable to unmarchal: %s", err)
-	}
-
-	if len(jsonData) < 1 {
-		return fmt.Errorf("[ERROR] JsonData is empty")
-	}
-
-	for _, v := range jsonData {
-		if v.Name == "robot$"+name {
-			d.Set("robot_id", strconv.Itoa(v.RobotID))
-		}
-	}
 	return nil
 }
 
@@ -143,9 +195,7 @@ func resourceRobotAccountUpdate(d *schema.ResourceData, m interface{}) error {
 func resourceRobotAccountDelete(d *schema.ResourceData, m interface{}) error {
 	apiClient := m.(*client.Client)
 	projectid := d.Get("project_id").(string)
-	robotid := d.Get("robot_id").(string)
-	url := pathRobot + "/" + projectid + "/robots/" + robotid
-	apiClient.SendRequest("DELETE", url, nil, 0)
-
-	return nil
+	url := pathRobot + "/" + projectid + "/robots/" + d.Id()
+	_, err := apiClient.SendRequest("DELETE", url, nil, 0)
+	return err
 }
